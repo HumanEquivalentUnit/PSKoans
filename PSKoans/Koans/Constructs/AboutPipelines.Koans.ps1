e08ad5622e1bcb027b092a3274ed4901171a739e
Joel <32407840+vexx32@users.noreply.github.com>
2018-11-06 13:27:37 -0500

    Rewording for clarity and accessibility

--- PSKoans/Koans/Constructs/AboutPipelines.Koans.ps1
+++ PSKoans/Koans/Constructs/AboutPipelines.Koans.ps1
@@ -5,38 +5,38 @@ param()
 <#
     Pipelines
 
-    PowerShell's pipeline structure is implemented as partly a language feature and
-    partly a function/cmdlet feature; you don't have to implement pipeline-supporting
+    PowerShell's pipeline structure is implemented partly as a language feature and
+    partly as a function/cmdlet feature; you don't have to implement pipeline-supporting
     logic in your functions, and not every cmdlet does.
 
     However, the vast majority of cmdlets do have pipeline-ready capabilities, and
     many module functions will often also implement pipeline logic.
 
     The pipeline is a feature that is used to streamline passing objects around, and
-    as a basic tenet of its functionality, it by default enumerates (i.e., breaks apart)
-    collections in order to be able to pass objects around one at a time and operate on
-    each in their turn.
+    as a part of its processing, it will break up arrays and other collections in order
+    to work with objects one after the other.
 
     The pipeline operates in stages:
 
         1. Begin {}
-            All cmdlets in the pipeline, in sequence, execute their begin{} blocks. Input from
-            directly-provided parameters can be used, but no input passed to pipeline parameters
-            is processed in this step. Output can be generated, and will be processed before any
-            other <process{}> steps.
+            Each command executes its begin{} blocks in order. Direct input from parameters can
+            be used, but no input passed to pipeline parameters is processed in this step.
+            Output can be generated, which will be processed before any other <process{}> steps.
         2. Process {}
-            The first statement or command is evaluated and the output passed into the <process{}>
-            block of the next function. The pipeline sequence executes once for each object sent in,
-            as output is emitted from one function to the next. Each function in turn takes its
-            input, and provides any output to the next function in the pipeline.
-            At the end of the pipeline, the resulting objects, if any, are collected and packaged as
-            an ArrayList for display or to be stored. If they are being stored, they are converted
-            into a standard [object[]] array.
+            The first statement or command is executed, and the output give to the <process{}>
+            block of the next command in line. The pipeline sequence executes once for each
+            object, and output from one function is used as input for the next. Each function
+            in turn takes the input, does its job, and provides any output to the next command.
         3. End {}
             As with begin, input passed to pipeline parameters is not typically available in this
             step. However, should the author wish to handle all input in bulk, it can be done by
             using the automatic $Input variable in the <end{}> step. Output may still be emitted,
-            and will be processed by the next pipeline after all precending <process{}> steps.
+            and will be processed by the next command's <process{}> block after all precending
+            <process{}> steps.
+
+            At the end of the pipeline, any resulting objects are packaged as an ArrayList for
+            display or to be stored. If the output results in a single object, it will be stored
+            as-is; otherwise, all output objects are wrapped in an array of type [object[]]
 
         Visualizing this process looks something like this:
 
